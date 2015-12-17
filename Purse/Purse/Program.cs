ee4e14fd3dc2a562f60901bd0699ffadf625f6c3
flylight1 <volodymyrzadorozhnyy109@gmail.com>
2015-12-17 09:39:57 +0200

    add businessCard dll

--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BusinessCard;
 namespace Purse
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            BusinessCardClass businesCard1 = new BusinessCardClass { Title = "businessCard1" };
+
         }
     }
 }
